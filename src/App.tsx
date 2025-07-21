73e1d511c9d4342a5e1ad87ba51c4602f37929e3
Kanchana K. <kanchanak.bandara@gmail.com>
2025-07-21 18:26:15 +0530

    Implemented scroll to top behavior for routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./Pages/Home";
 import Projects from "./Pages/Projects";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       <main className="font-inter mx-auto min-h-screen w-4/5 max-w-3xl text-gray-800">
+        <ScrollToTop />
         <Routes>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
